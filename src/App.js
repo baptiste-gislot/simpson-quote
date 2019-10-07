d7444d839ff93f17ede60460578324c45ebbfc06
KobeSan <baptiste.gislot@gmail.com>
2019-10-07 16:20:28 +0200

    Ajout des QuotesCards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import NavBar from './components/navbar';
+import QuoteCards from './components/QuoteCards';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <QuoteCards image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638" character="Bart Simpson" quote="Nothing you say can upset us. We're the MTV generation."/>  
+      <QuoteCards /> 
     </div>
   );
 }
