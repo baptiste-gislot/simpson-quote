5ee66230d1facf07a21c7f1ab39e530feb9d92c5
KobeSan <baptiste.gislot@gmail.com>
2019-10-07 14:19:01 +0200

    Création de la navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function NavBar() {
+    return (
+        <nav className="Navbar">
+            <h1>Simpsons' Quotes</h1>
+        </nav>
+    );
+}
+
+export default NavBar;
