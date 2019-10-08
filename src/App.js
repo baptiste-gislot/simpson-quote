6f9e7c2963776eb51386167716dd1b5910d28ab3
KobeSan <baptiste.gislot@gmail.com>
2019-10-08 09:18:39 +0200

    Modif App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <NavBar />
       <QuoteCards image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638" character="Bart Simpson" quote="Nothing you say can upset us. We're the MTV generation."/>  
-      <QuoteCards /> 
+      <QuoteCards  image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523" character="Ralph Wiggum" quote="Hi, Super Nintendo Chalmers!"/> 
     </div>
   );
 }
