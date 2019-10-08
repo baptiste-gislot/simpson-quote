d0e9f1f74642f81941a10b6bc10ee2914f727487
KobeSan <baptiste.gislot@gmail.com>
2019-10-08 11:23:12 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import NavBar from './components/navbar';
-import QuoteCards from './components/QuoteCards';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <QuoteCards image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638" character="Bart Simpson" quote="Nothing you say can upset us. We're the MTV generation."/>  
-      <QuoteCards  image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523" character="Ralph Wiggum" quote="Hi, Super Nintendo Chalmers!"/> 
+      <QuoteList />
     </div>
   );
 }
